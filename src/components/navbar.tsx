134324f9cd83de3f668e9d607c477f242a4f2138
jaydencolaco <jaydencolaco20@gmail.com>
2025-05-18 18:50:20 +0530

    for the logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Building, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useState } from "react";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { Menu } from "lucide-react";
 
 export function Navbar() {
   const pathname = usePathname();
@@ -19,19 +21,45 @@ export function Navbar() {
   ];
 
   const [open, setOpen] = useState(false);
+  const { theme } = useTheme();
+
+  const logoSrc = theme === "dark" ? "/logodark.png" : "/logo.png";
+
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
-        <div className="flex items-center gap-2">
+        {/* <div className="flex items-center gap-2">
           <Link
             href="/"
             className="flex items-center gap-2 font-bold text-xl text-primary"
           >
-            <Building className="h-6 w-6" />
+            <img
+                  src="/logo.png"
+                  alt="logo"
+                  className="rounded-lg object-cover"
+                  width={100}
+                  height={100}
+                />
             <span className="hidden sm:inline">WealthBridge Consulting</span>
             <span className="sm:hidden">WealthBridge</span>
           </Link>
-        </div>
+        </div> */}
+        <div className="flex items-center gap-2">
+      <Link
+        href="/"
+        className="flex items-center gap-2 font-bold text-xl text-primary"
+      >
+        <Image
+          src={logoSrc}
+          alt="logo"
+          className="rounded-lg object-cover"
+          width={100}
+          height={100}
+        />
+        <span className="hidden sm:inline">WealthBridge Consulting</span>
+        <span className="sm:hidden">WealthBridge</span>
+      </Link>
+    </div>
         <nav className="hidden md:flex gap-6">
           {navItems.map((item) => (
             <Link
